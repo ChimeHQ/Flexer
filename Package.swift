44e278ffc623dd7e90f14a25a00a095ee0503d30
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-04-28 07:22:02 -0400

    Move to standard SPM layout

--- Package.swift
+++ Package.swift
@@ -1,16 +1,32 @@
-// swift-tools-version:5.0
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "Flexer",
-    platforms: [.macOS(.v10_12), .iOS(.v10)],
+    platforms: [
+		.macOS(.v10_15),
+		.iOS(.v13),
+		.tvOS(.v13),
+		.watchOS(.v6),
+		.macCatalyst(.v13)
+	],
     products: [
         .library(name: "Flexer", targets: ["Flexer"]),
     ],
     dependencies: [],
     targets: [
-        .target(name: "Flexer", dependencies: [], path: "Flexer/"),
-        .testTarget(name: "FlexerTests", dependencies: ["Flexer"], path: "FlexerTests/"),
+        .target(name: "Flexer", dependencies: []),
+        .testTarget(name: "FlexerTests", dependencies: ["Flexer"]),
     ]
 )
+
+let swiftSettings: [SwiftSetting] = [
+	.enableExperimentalFeature("StrictConcurrency")
+]
+
+for target in package.targets {
+	var settings = target.swiftSettings ?? []
+	settings.append(contentsOf: swiftSettings)
+	target.swiftSettings = settings
+}

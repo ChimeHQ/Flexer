4c22a419c7df6cd6c30f858bb339ef9346965ad5
Ralph Kühnert <kuehnert.ralph@gmail.com>
2025-04-10 16:07:29 +0200

    adjust test to reflect new nil behavior
    Signed-off-by: Ralph Kühnert <kuehnert.ralph@gmail.com>

--- Tests/FlexerTests/BasicTextCharacterLexerTests.swift
+++ Tests/FlexerTests/BasicTextCharacterLexerTests.swift
@@ -1,45 +1,45 @@
-import XCTest
 @testable import Flexer
+import XCTest
 
 class BasicTextCharacterLexerTests: XCTestCase {
     func testPeekTwiceReturnsSameValue() {
         let string = "\t"
         var lexer = BasicTextCharacterLexer(string: string)
 
-        let token = BasicTextCharacter(kind: .tab, range: NSRange(0..<1), in: string)
+        let token = BasicTextCharacter(kind: .tab, range: NSRange(0 ..< 1), in: string)
 
         XCTAssertEqual(lexer.peek(), token)
         XCTAssertEqual(lexer.peek(), token)
     }
 
     func testNextReturnsDifferentValue() {
-        let string: String = "\t"
+        let string = "\t"
         var lexer = BasicTextCharacterLexer(string: string)
 
         XCTAssertNotNil(lexer.peek())
-        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .tab, range: NSRange(0..<1), in: string))
+        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .tab, range: NSRange(0 ..< 1), in: string))
         XCTAssertNil(lexer.peek())
     }
-	
-	func testPeekFurtherAndThenCloser() {
-		let string: String = "a1"
-		var lexer = BasicTextCharacterLexer(string: string)
 
-		XCTAssertEqual(lexer.peek(distance: 2), BasicTextCharacter(kind: .digit, range: NSRange(1..<2), in: string))
-		XCTAssertEqual(lexer.peek(distance: 1), BasicTextCharacter(kind: .lowercaseLetter, range: NSRange(0..<1), in: string))
-		XCTAssertEqual(lexer.peek(distance: 2), BasicTextCharacter(kind: .digit, range: NSRange(1..<2), in: string))
-	}
+    func testPeekFurtherAndThenCloser() {
+        let string = "a1"
+        var lexer = BasicTextCharacterLexer(string: string)
+
+        XCTAssertEqual(lexer.peek(distance: 2), BasicTextCharacter(kind: .digit, range: NSRange(1 ..< 2), in: string))
+        XCTAssertEqual(lexer.peek(distance: 1), BasicTextCharacter(kind: .lowercaseLetter, range: NSRange(0 ..< 1), in: string))
+        XCTAssertEqual(lexer.peek(distance: 2), BasicTextCharacter(kind: .digit, range: NSRange(1 ..< 2), in: string))
+    }
 }
 
 extension BasicTextCharacterLexerTests {
     func testPeekNextPerformance() {
         var string = ""
 
-        for _ in 0..<10000 {
+        for _ in 0 ..< 10000 {
             string += "abc 123 ;[];^^%\n"
         }
 
-        self.measure {
+        measure {
             var lexer = BasicTextCharacterLexer(string: string)
 
             while lexer.peek() != nil {
@@ -54,7 +54,7 @@ extension BasicTextCharacterLexerTests {
         let string = "a"
         var lexer = BasicTextCharacterLexer(string: string)
 
-        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .lowercaseLetter, range: NSRange(0..<1), in: string))
+        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .lowercaseLetter, range: NSRange(0 ..< 1), in: string))
         XCTAssertNil(lexer.next())
     }
 
@@ -62,9 +62,9 @@ extension BasicTextCharacterLexerTests {
         let string = "ABC"
         var lexer = BasicTextCharacterLexer(string: string)
 
-        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .uppercaseLetter, range: NSRange(0..<1), in: string))
-        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .uppercaseLetter, range: NSRange(1..<2), in: string))
-        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .uppercaseLetter, range: NSRange(2..<3), in: string))
+        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .uppercaseLetter, range: NSRange(0 ..< 1), in: string))
+        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .uppercaseLetter, range: NSRange(1 ..< 2), in: string))
+        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .uppercaseLetter, range: NSRange(2 ..< 3), in: string))
         XCTAssertNil(lexer.next())
     }
 
@@ -72,21 +72,30 @@ extension BasicTextCharacterLexerTests {
         let string = "123"
         var lexer = BasicTextCharacterLexer(string: string)
 
-        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .digit, range: NSRange(0..<1), in: string))
-        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .digit, range: NSRange(1..<2), in: string))
-        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .digit, range: NSRange(2..<3), in: string))
+        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .digit, range: NSRange(0 ..< 1), in: string))
+        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .digit, range: NSRange(1 ..< 2), in: string))
+        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .digit, range: NSRange(2 ..< 3), in: string))
         XCTAssertNil(lexer.next())
     }
 
     func testNextUntilWithNoMatch() {
-        let string = " a "
+        let string = " ab "
         var lexer = BasicTextCharacterLexer(string: string)
 
-        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .space, range: NSRange(0..<1), in: string))
-        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .lowercaseLetter, range: NSRange(1..<2), in: string))
+        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .space, range: NSRange(0 ..< 1), in: string))
+        XCTAssertEqual(lexer.next(), BasicTextCharacter(kind: .lowercaseLetter, range: NSRange(1 ..< 2), in: string))
 
+        // The lexer is spooled to the "a" position.
+        let bToken = lexer.nextUntil(notIn: [.lowercaseLetter])
+        // So the return token must be the last one matching e.g. "b".
+        XCTAssertEqual(bToken, BasicTextCharacter(kind: .lowercaseLetter, range: NSRange(2 ..< 3), in: string))
+
+        // The lexer is already spooled to the "b" position.
+        // next() would be " ".
         let token = lexer.nextUntil(notIn: [.lowercaseLetter])
 
-        XCTAssertEqual(token, BasicTextCharacter(kind: .space, range: NSRange(2..<3), in: string))
+        // But our stop condition is notIn: [.lowercaseLetter].
+        // So we need to immediately stop.
+        XCTAssertNil(token)
     }
 }

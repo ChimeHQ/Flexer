b8fb79efe3ea65cbb2f7e864042ebce47fada488
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-05-05 08:16:24 -0400

    Add .swift-format file and run it on TokenProtocol

--- Sources/Flexer/TokenProtocol.swift
+++ Sources/Flexer/TokenProtocol.swift
@@ -9,82 +9,82 @@
 import Foundation
 
 public protocol TokenProtocol: Comparable, CustomStringConvertible {
-    associatedtype Kind: Hashable
-    associatedtype Index: Comparable
+	associatedtype Kind: Hashable
+	associatedtype Index: Comparable
 
-    var range: Range<Index> { get }
-    var kind: Kind { get }
+	var range: Range<Index> { get }
+	var kind: Kind { get }
 
-    init(kind: Kind, range: Range<Index>)
+	init(kind: Kind, range: Range<Index>)
 }
 
-public extension TokenProtocol where Index == String.Index {
-    init?(kind: Kind, range: NSRange, in string: String) {
-        guard let stringRange = Range<String.Index>(range, in: string) else {
-            return nil
-        }
+extension TokenProtocol where Index == String.Index {
+	public init?(kind: Kind, range: NSRange, in string: String) {
+		guard let stringRange = Range<String.Index>(range, in: string) else {
+			return nil
+		}
 
-        self.init(kind: kind, range: stringRange)
-    }
+		self.init(kind: kind, range: stringRange)
+	}
 
-    init?(kind: Kind, start: BasicTextCharacter, end: BasicTextCharacter?) {
-        guard let end = end else { return nil }
+	public init?(kind: Kind, start: BasicTextCharacter, end: BasicTextCharacter?) {
+		guard let end = end else { return nil }
 
-        self.init(kind: kind, range: start.startIndex..<end.endIndex)
-    }
-    
-    func nsRange(in string: String) -> NSRange {
-        return NSRange(range, in: string)
-    }
-}
+		self.init(kind: kind, range: start.startIndex..<end.endIndex)
+	}
 
-public extension TokenProtocol {
-    var startIndex: Index {
-        return range.lowerBound
-    }
-
-    var endIndex: Index {
-        return range.upperBound
-    }
+	public func nsRange(in string: String) -> NSRange {
+		return NSRange(range, in: string)
+	}
 }
 
 extension TokenProtocol {
-    public static func < (lhs: Self, rhs: Self) -> Bool {
-        return lhs.startIndex < rhs.startIndex
-    }
+	public var startIndex: Index {
+		return range.lowerBound
+	}
+
+	public var endIndex: Index {
+		return range.upperBound
+	}
 }
 
 extension TokenProtocol {
-    public var description: String {
-        return "<\(type(of: self)) \(String(describing: kind)) \(String(describing: range))>"
-    }
+	public static func < (lhs: Self, rhs: Self) -> Bool {
+		return lhs.startIndex < rhs.startIndex
+	}
 }
 
-public extension LookAheadSequence where Base.Element: TokenProtocol {
-    mutating func nextUntil(notIn set: Set<Element.Kind>) -> Base.Element? {
-        return nextUntil({ set.contains($0.kind) == false })
-    }
-
-    mutating func nextUntil(in set: Set<Element.Kind>) -> Base.Element? {
-        return nextUntil({ set.contains($0.kind) })
-    }
+extension TokenProtocol {
+	public var description: String {
+		return "<\(type(of: self)) \(String(describing: kind)) \(String(describing: range))>"
+	}
 }
 
-public extension LookAheadSequence where Base.Element: TokenProtocol {
-    var tokens: Base {
-        return baseSequence
-    }
+extension LookAheadSequence where Base.Element: TokenProtocol {
+	public mutating func nextUntil(notIn set: Set<Element.Kind>) -> Base.Element? {
+		return nextUntil({ set.contains($0.kind) == false })
+	}
+
+	public mutating func nextUntil(in set: Set<Element.Kind>) -> Base.Element? {
+		return nextUntil({ set.contains($0.kind) })
+	}
+}
+
+extension LookAheadSequence where Base.Element: TokenProtocol {
+	public var tokens: Base {
+		return baseSequence
+	}
 }
 
 public struct Token<TokenKind: Hashable>: TokenProtocol {
-    public typealias Kind = TokenKind
-    public typealias Index = String.Index
+	public typealias Kind = TokenKind
+	public typealias Index = String.Index
 
-    public var range: Range<Index>
-    public var kind: Kind
+	public var range: Range<Index>
+	public var kind: Kind
 
-    public init(kind: TokenKind, range: Range<Index>) {
-        self.kind = kind
-        self.range = range
-    }
+	public init(kind: TokenKind, range: Range<Index>) {
+		self.kind = kind
+		self.range = range
+	}
 }
